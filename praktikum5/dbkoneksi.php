17c678b5bfa873cb2e572fdaacc011406cb1778a
4nd1n <andinidini02222@gmail.com>
2025-03-21 16:37:43 +0700

    Add files via upload

---
+++ praktikum5/dbkoneksi.php
@@ -0,0 +1,14 @@
+<?php 
+$host = "localshot";
+$dbname= 'db_puskesmas';
+$dbname = "root";
+$password = '';
+
+$dsn = "mysql:host=$host;dbname=$name";
+
+$pdo = new PDO($dsn, $username, $password);
+?>
+
+
+
+  
